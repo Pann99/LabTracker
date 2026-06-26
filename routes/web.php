97ac92dd77455d460a2fd2551170933e042d6ef5
= <=>
2026-06-26 21:53:51 +0700

    Menambah controller dan route CRUD untuk modul Alat

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlatController;
+use App\Http\Controllers\PeminjamController;
+use App\Http\Controllers\PeminjamanController;
+use App\Models\Alat;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', fn() => redirect()->route('alat.index'));
+
+Route::get('/api/alat', function () {
+    return response()->json(Alat::all());
 });
+
+Route::resource('alat', AlatController::class)->except(['show']);
+Route::resource('peminjam', PeminjamController::class)->except(['show']);
+Route::resource('peminjaman', PeminjamanController::class)->only(['index', 'create', 'store']);
+Route::patch('/peminjaman/{peminjaman}/kembalikan', [PeminjamanController::class, 'update'])->name('peminjaman.update');
