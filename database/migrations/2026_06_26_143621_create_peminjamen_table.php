834170322a503bef8bb384a8aa6a106faa7c47cd
= <=>
2026-06-26 21:43:24 +0700

    Menambah migration alat, peminjam, dan peminjaman

---
+++ database/migrations/2026_06_26_143621_create_peminjamen_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('alat_id')->constrained('alats')->onDelete('cascade');
+            $table->foreignId('peminjam_id')->constrained('peminjams')->onDelete('cascade');
+            $table->date('tanggal_pinjam');
+            $table->date('tanggal_kembali')->nullable();
+            $table->enum('status', ['dipinjam', 'dikembalikan', 'terlambat'])->default('dipinjam');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+};
